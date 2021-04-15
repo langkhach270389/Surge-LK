a0f5202a7c83d1cd620d96848e5504cf31900f63
Lãng Khách <langkhach270389@users.noreply.github.com>
2021-04-16 05:21:57 +0700

    AutoNextDNS.sgmodule

---
+++ scripts/langkhach/autodns.js
@@ -0,0 +1,42 @@
+/*
+@langkhach
+AutoNextDNS - HomeDNS
+Surge TF 4.11.0 build 1910
+*/
+const sgmodule = "NextDNS-Utra-Low";
+const path = "v1/modules";
+var body = {};
+
+getModuleStatus(sgmodule).then(main);
+
+function main(enable) {
+  console.log(getModuleStatus(sgmodule));
+  let home = $network.wifi.ssid == "Tầng 3" || $network.wifi.ssid == "VNPT";
+  if (home && enable) {
+    //At home, it will enable nextDNS => close
+    $notification.post("Close NextDNS ", "", "");
+    enableModule(false);
+  } else if (!home && !enable) {
+    //Not at home, but didn't enable nextdns =>
+    $notification.post("Enable NextDNS ", "", "");
+    enableModule(true);
+  } else {
+    //Repeat trigger => End
+    //$notification.post("Repeat trigger ","","")
+    $done();
+  }
+}
+
+function getModuleStatus(sgmodule) {
+  return new Promise(resolve => {
+    $httpAPI("GET", path, body, resp => {
+      let enabled = resp.enabled;
+      resolve(enabled.includes(sgmodule));
+    });
+  });
+}
+
+function enableModule(enable) {
+  body[sgmodule] = enable;
+  $httpAPI("POST", path, body, () => $done());
+}
