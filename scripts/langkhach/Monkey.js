cf964fa0a21f21aa5d1c1d8004490c87012df0d1
Kim Văn HIỆP <hiepkimcdtk55@gmail.com>
2022-12-15 21:47:03 +0700

    Monkey

---
+++ scripts/langkhach/Monkey.js
@@ -0,0 +1,12 @@
+let obj = JSON.parse($response.body);
+
+obj.data.purchased=
+{
+      "product_list": [
+      ],
+      "is_active": true,
+      "time_expire": 0,
+      "pay_inapp": true,
+      "is_free": false
+    };
+$done({body: JSON.stringify(obj)});
