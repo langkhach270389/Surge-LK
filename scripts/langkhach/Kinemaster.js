382fc66fa443fda28f5d3f68cc032677338c7cc7
Kim Văn HIỆP <hiepkimcdtk55@gmail.com>
2022-12-16 14:20:01 +0700

    Kinemaster

---
+++ scripts/langkhach/Kinemaster.js
@@ -0,0 +1,8 @@
+let obj = {
+  "expiresDate": 4101073360000,
+  "isSubscribed": true,
+  "productId": "com.kinemaster.kios.annual.ia4",
+  "isAutoRenew": true,
+  "platform": "APPSTORE"
+};
+$done({body: JSON.stringify(obj)});
